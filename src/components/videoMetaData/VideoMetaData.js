f518ba8ff4c0690a42765fd12d5b0d0c3330e2b2
beniamanjaro <vrajitorbeny10@gmail.com>
2022-05-14 15:09:58 +0300

    added video details and description

--- src/components/videoMetaData/VideoMetaData.js
+++ src/components/videoMetaData/VideoMetaData.js
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import moment from "moment";
+import numeral from "numeral";
+import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
 import "./_videoMetaData.scss";
+import { getChannelDetailsById } from "../../redux/actions/channel.action";
+import { useDispatch, useSelector } from "react-redux";
 
-const VideoMetaData = () => {
-  return <div>VideoMetaData</div>;
+const VideoMetaData = ({ video }) => {
+  const { snippet, statistics } = video;
+  const { title, publishedAt, description, channelId } = snippet;
+  const { viewCount, likeCount } = statistics;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getChannelDetails = async () => {
+      await dispatch(getChannelDetailsById(channelId));
+    };
+    console.log(channelId);
+    getChannelDetails();
+  }, [dispatch, channelId]);
+
+  return (
+    <div className="video__metaData">
+      <div className="video__metaData__top">
+        <div className="video__metaData__top__title">{title}</div>
+        <div className="video__metaData__top__stats">
+          <div className="video__metaData__top__stats__left">
+            <span>{numeral(viewCount).format("0.a")} Views •</span>
+            <span>{moment(publishedAt).fromNow()}</span>
+          </div>
+          <div className="video__metaData__top__stats__right">
+            <div className="video__metaData__top__stats__right__likes">
+              <AiOutlineLike style={{ height: "2rem", width: "2rem" }} />
+              <span>{numeral(likeCount).format("0.a")}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="video__metaData__description">
+        <div className="video__metaData__description__channel">channel</div>
+        <div className="video__metaData__description__text">
+          {description.split("\n").map((l, i, arr) => {
+            const line = <span key={i}>{l}</span>;
+            if (i === arr.length - 1) {
+              return line;
+            } else {
+              return [line, <br key={i + "br"} />];
+            }
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoMetaData;
--- src/screens/watchScreen/WatchScreen.js
+++ src/screens/watchScreen/WatchScreen.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import VideoHorizontal from "../../components/videoHorizontal/VideoHorizontal";
 import VideoMetaData from "../../components/videoMetaData/VideoMetaData";
+import Comments from "../../components/comments/Comments";
 import "./_watchScreen.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { getVideoDetailsById } from "../../redux/actions/videos.action";
 
 const WatchScreen = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const call = async () => {
+      await dispatch(getVideoDetailsById(id));
+    };
+    call();
+  }, [dispatch, id]);
+
+  const { video, loading } = useSelector((state) => state.videoDetails);
 
   return (
     <Row>
@@ -14,14 +27,15 @@ const WatchScreen = () => {
         <div className="watchScreen__player">
           <iframe
             src={`https://www.youtube.com/embed/${id}`}
-            frameborder="0"
-            title="My Video"
+            frameBorder="0"
+            title={video?.snippet?.title}
             allowFullScreen
             width="100%"
             height="100%"
           ></iframe>
         </div>
-        <VideoMetaData />
+        {!loading ? <VideoMetaData video={video} /> : <h5>Loading</h5>}
+        <Comments />
       </Col>
       <Col lg={4}>
         {[...Array(10)].map(() => (
